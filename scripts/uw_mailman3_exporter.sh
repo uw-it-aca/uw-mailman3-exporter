8a1ed63aa1a4cbf5fd49b9416c72f27060b04b34
mike seibel <mikes@washington.edu>
2023-10-25 11:15:29 -0700

    exporter args

--- scripts/uw_mailman3_exporter.sh
+++ scripts/uw_mailman3_exporter.sh
@@ -2,4 +2,4 @@
 
 source /app/bin/activate
 
-python /app/mailman_exporter.py -u "$MAILMAN_ADMIN_USER" -p "$MAILMAN_ADMIN_PASSWORD"
+python /app/mailman_exporter.py -l "localhost:9091" -m "${HOSTNAME}:8080"  -u "$MAILMAN_ADMIN_USER" -p "$MAILMAN_ADMIN_PASSWORD" --log-level=debug
