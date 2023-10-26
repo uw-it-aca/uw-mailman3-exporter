8467b2fc4c7fa28617fb6053852005bf778e4674
mike seibel <mikes@washington.edu>
2023-10-26 10:52:29 -0700

    proper rest endpoint

--- scripts/uw_mailman3_exporter.sh
+++ scripts/uw_mailman3_exporter.sh
@@ -2,4 +2,4 @@
 
 source /app/bin/activate
 
-python /app/mailman_exporter.py -l "localhost:9091" -m "${HOSTNAME::-2}:80"  -u "$MAILMAN_ADMIN_USER" -p "$MAILMAN_ADMIN_PASSWORD" --log-level=debug
+python /app/mailman_exporter.py -l "localhost:9091" -m "http://${HOSTNAME::-2}:80"  -u "$MAILMAN_ADMIN_USER" -p "$MAILMAN_ADMIN_PASSWORD"
